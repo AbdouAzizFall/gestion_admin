1e1b3c52d5af80785afa41327bcfec1a672ee496
AbdouAzizFall <fallaziz699@gmail.com>
2024-06-14 13:47:51 +0000

    Modification de la page index

--- index.php
+++ index.php
@@ -4,47 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestion administrative d'une ecole</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-        }
-        .header {
-            display: flex;
-            align-items: center;
-            padding: 10px;
-            background-color: #f0f0f0;
-        }
-        .logo {
-            margin-right: 20px;
-        }
-        .options {
-            display: flex;
-            list-style-type: none;
-            margin: 0;
-            padding: 0;
-        }
-        .options li {
-            margin-right: 20px;
-        }
-    </style>
 </head>
 <body>
-    <div class="header">
-        <div class="logo">
-            <img src="img/logo.jpg" alt="image" style="height: 100px;">
-        </div>
-        <hr style="margin: 0 20px; height: 2px; border: none; background-color: #333;">
-        <ul class="options">
-            <li><a href="#">Gestion filière</a></li>
-            <li><a href="#">Gestion module</a></li>
-            <li><a href="#">Gestion prof</a></li>
-            <li><a href="#">Gestion étudiant</a></li>
-        </ul>
-    </div>
-
-    <!-- Ajoutez le reste de votre contenu ici -->
-
+    <nav>
+        <img src="img/logo.jpg" alt="image" style="height: 50px;">
+        <class class="menu">
+            <ul>
+                <li><a href="#"></a>Gestion des filieres</li>
+                <li><a href="#"></a>Gestion des modules</li>
+                <li><a href="#"></a>gestion des profs</li>
+                <li><a href="#"></a>Gestion des etudiants</li>
+            </ul>
+        </class>
+    </nav>
 </body>
 </html>
